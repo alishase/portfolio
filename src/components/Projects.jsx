741cb1196709c4f32aea8ac529d18d59905430a5
alishase <maksimzorin10@gmail.com>
2024-05-07 23:15:04 +0200

    first-commit

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -3,27 +3,6 @@ import dots from "../assets/Dots.png";
 import Rectangle from "../assets/Rectangle2.png";
 const Projects = ({ cards }) => {
     return (
-<<<<<<< HEAD
-        <div className="projects--section">
-            <div className="projects" id="projects">
-                <img src={dots} alt="" className="bagDetail" />
-                <img src={dots} alt="" className="bagDetailR" />
-                <div className="upper">
-                    <div className="sectionTitle">
-                        <h1>
-                            <span className="symb">#</span>projects
-                        </h1>
-                        <hr className="upperSection" />
-                    </div>
-                    <a href="#" className="viewAll">
-                        View all ~~{">"}
-                    </a>
-                </div>
-                <div className="cardsContainer">
-                    {cards.map((card, index) => (
-                        <Card key={index} card={card} />
-                    ))}
-=======
         <div className="projects" id="projects">
             <img src={dots} alt="" className="bagDetail" />
             <img src={dots} alt="" className="bagDetailR" />
@@ -33,7 +12,6 @@ const Projects = ({ cards }) => {
                         <span className="symb">#</span>projects
                     </h1>
                     <hr className="upperSection" />
->>>>>>> fecd039af8c8f86506c4cd2532890123fd0a8b6d
                 </div>
             </div>
             <div className="primaryBtn">View all projects</div>
